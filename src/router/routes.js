fff1a0bc4ba09efcbf9846477469f592f2e2131b
hua5363210 <1530860431@qq.com>
2019-09-03 15:14:01 +0800

    更新

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,75 +1,86 @@
-import Login from '@/views/Login.vue'
-import NotFound from '@/views/404.vue'
+
 import Home from '@/views/Home.vue'
-import Main from '@/views/Main.vue'
-import Table from '@/views/nav1/Table.vue'
-import Form from '@/views/nav1/Form.vue'
-import user from '@/views/nav1/user.vue'
 import Page4 from '@/views/nav2/Page4.vue'
 import Page5 from '@/views/nav2/Page5.vue'
 import Page6 from '@/views/nav3/Page6.vue'
 import echarts from '@/views/charts/echarts.vue'
 
-let routes = [
-    {
-        path: '/login',
-        component: Login,
-        name: '',
-        hidden: true
-    },
-    {
-        path: '/404',
-        component: NotFound,
-        name: '',
-        hidden: true
-    },
-    //{ path: '/main', component: Main },
-    {
-        path: '/',
-        component: Home,
-        name: '导航一',
-        iconCls: 'el-icon-message',//图标样式class
-        children: [
-            { path: '/main', component: Main, name: '主页', hidden: true },
-            { path: '/table', component: Table, name: 'Table' },
-            { path: '/form', component: Form, name: 'Form' },
-            { path: '/user', component: user, name: '列表' },
-        ]
-    },
-    // {
-    //     path: '/',
-    //     component: Home,
-    //     name: '导航二',
-    //     iconCls: 'fa fa-id-card-o',
-    //     children: [
-    //         { path: '/page4', component: Page4, name: '页面4' },
-    //         { path: '/page5', component: Page5, name: '页面5' }
-    //     ]
-    // },
-    // {
-    //     path: '/',
-    //     component: Home,
-    //     name: '',
-    //     iconCls: 'fa fa-address-card',
-    //     leaf: true,//只有一个节点
-    //     children: [
-    //         { path: '/page6', component: Page6, name: '导航三' }
-    //     ]
-    // },
-    // {
-    //     path: '/',
-    //     component: Home,
-    //     name: 'Charts',
-    //     iconCls: 'fa fa-bar-chart',
-    //     children: [
-    //         { path: '/echarts', component: echarts, name: 'echarts' }
-    //     ]
-    // },
-    {
-        path: '*',
-        hidden: true,
-        redirect: { path: '/404' }
-    }
+let routes = [{
+		path: '/login',
+		component: resolve => require(['@/views/Login.vue'],resolve),
+		name: '',
+		hidden: true
+	},
+	{
+		path: '/404',
+		component: resolve => require(['@/views/404.vue'],resolve),
+		name: '',
+		hidden: true
+	},
+	//{ path: '/main', component: Main },
+	{
+		path: '/',
+		component: Home,
+		name: '会员管理',
+		iconCls: 'el-icon-message', //图标样式class
+		children: [{
+				path: '/main',
+				component: resolve => require(['@/views/Main.vue'],resolve),
+				name: '设置会员卡种'
+			},
+			{
+				path: '/table',
+				component: resolve => require(['@/views/nav1/Table.vue'],resolve),
+				name: '会员卡发放管理'
+			},
+			{
+				path: '/form',
+				component: resolve => require(['@/views/nav1/Form.vue'],resolve),
+				name: '会员卡充值管理'
+			},
+			{
+				path: '/user',
+				component: resolve => require(['@/views/nav1/user.vue'],resolve),
+				name: '会员卡消费记录'
+			},
+		]
+	},
+	// {
+	//     path: '/',
+	//     component: Home,
+	//     name: '导航二',
+	//     iconCls: 'fa fa-id-card-o',
+	//     children: [
+	//         { path: '/page4', component: Page4, name: '页面4' },
+	//         { path: '/page5', component: Page5, name: '页面5' }
+	//     ]
+	// },
+	// {
+	//     path: '/',
+	//     component: Home,
+	//     name: '',
+	//     iconCls: 'fa fa-address-card',
+	//     leaf: true,//只有一个节点
+	//     children: [
+	//         { path: '/page6', component: Page6, name: '导航三' }
+	//     ]
+	// },
+	// {
+	//     path: '/',
+	//     component: Home,
+	//     name: 'Charts',
+	//     iconCls: 'fa fa-bar-chart',
+	//     children: [
+	//         { path: '/echarts', component: echarts, name: 'echarts' }
+	//     ]
+	// },
+	{
+		path: '*',
+		hidden: true,
+		redirect: {
+			path: '/404'
+		}
+	}
 ];
 
 export default routes;
